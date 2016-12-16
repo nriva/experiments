1c9588a6acfd283bc6375ed0b893c7871a90fa19
CADIT\riva <nicola.riva@caditgroup.com>
2016-12-16 15:09:01 +0100

    Primo commit

---
+++ loggerexamples/src/main/java/com/nrsoft/loggerexamples/Log4jExample.java
@@ -0,0 +1,28 @@
+package com.nrsoft.loggerexamples;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ *
+ * Log4jExample
+ * Esempio di uso di log4j2
+ * Usa il file log4j2.xml
+ *
+ */
+public class Log4jExample 
+{
+	
+	final static Logger logger = LogManager.getLogger(Log4jExample.class);
+	
+    public static void main( String[] args )
+    {
+     
+    	logger.error("testo di errore");
+    	logger.warn("testo di warning");
+    	logger.info("testo di info");
+    	logger.debug("testo di debug");
+    	logger.trace("testo di trace");
+    	
+    }
+}
