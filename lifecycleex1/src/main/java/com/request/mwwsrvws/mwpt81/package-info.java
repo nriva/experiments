c8bae06afb7a2045759bebab0ba4fbaa7f23caa7
CADIT\riva <nicola.riva@caditgroup.com>
2017-10-30 12:35:07 +0100

    Semplificazione progetto

--- lifecycleex1/src/main/java/com/request/mwwsrvws/mwpt81/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.MWPT81.MWWSRVWS.Request.com", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.request.mwwsrvws.mwpt81;
--- lifecycleex1/src/main/java/com/response/mwwsrvws/mwpt81/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.MWPT81.MWWSRVWS.Response.com", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.response.mwwsrvws.mwpt81;
